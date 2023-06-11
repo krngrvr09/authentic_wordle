a0d86116b0bbd21f9ac964e4b92b4821015ec759
Karan Grover <krngrvr09@gmail.com>
2023-06-11 22:10:55 +0000

    Created a createGame lambda function and API endpoint

--- wordle_cdk/wordle_cdk_stack.py
+++ wordle_cdk/wordle_cdk_stack.py
@@ -13,11 +13,19 @@ class WordleCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        my_lambda = _lambda.Function(
+        create_user_lambda = _lambda.Function(
             self, 'CreateUser',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
             handler='createUser.handler',
+        )
+        # Create the Lambda function for creating a game
+        create_game_lambda = _lambda.Function(
+            self,
+            "CreateGameLambda",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="createGame.handler",
+            code=_lambda.Code.from_asset("lambda")
         )
 
         api = apigw.RestApi(
@@ -27,12 +35,20 @@ class WordleCdkStack(Stack):
             description="API for CdkProject",
             deploy_options={
                 "stage_name": "v1"
-            }
+            },
+            endpoint_types=[apigw.EndpointType.REGIONAL]
         )
 
          # Create the `/users` resource
         users_resource = api.root.add_resource("users")
 
-        # Add a POST method to the `/users` resource and connect it to the Lambda function
-        create_user_integration = apigw.LambdaIntegration(my_lambda)
+        # Add a POST method to the `/users` resource and connect it to the create user Lambda function
+        create_user_integration = apigw.LambdaIntegration(create_user_lambda)
         users_resource.add_method("POST", create_user_integration)
+
+        # Create the `/users/{user_id}/games` resource
+        users_games_resource = users_resource.add_resource("{user_id}").add_resource("games")
+
+        # Add a POST method to the `/users/{user_id}/games` resource and connect it to the create game Lambda function
+        create_game_integration = apigw.LambdaIntegration(create_game_lambda)
+        users_games_resource.add_method("POST", create_game_integration)
