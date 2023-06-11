2bfb8198d380ba74e2e5d4746a0ef7f060b63059
Karan Grover <krngrvr09@gmail.com>
2023-06-11 22:37:35 +0000

    AddedAPI method to make a guess

--- wordle_cdk/wordle_cdk_stack.py
+++ wordle_cdk/wordle_cdk_stack.py
@@ -5,7 +5,10 @@ from aws_cdk import (
     aws_iam as iam,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_dynamodb as dynamodb,
 )
+
+# from aws_cdk import core
 
 
 class WordleCdkStack(Stack):
@@ -13,20 +16,53 @@ class WordleCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # # Create the DynamoDB game table
+        # gameTable = dynamodb.Table(
+        #     self,
+        #     "GameTable",
+        #     partition_key=dynamodb.Attribute(name="game_id", type=dynamodb.AttributeType.STRING),
+        #     removal_policy=core.RemovalPolicy.DESTROY
+        # )
+
+        # # Create the DynamoDB user table
+        # userTable = dynamodb.Table(
+        #     self,
+        #     "UserTable",
+        #     partition_key=dynamodb.Attribute(name="user_id", type=dynamodb.AttributeType.STRING),
+        #     removal_policy=core.RemovalPolicy.DESTROY
+        # )
+
+
         create_user_lambda = _lambda.Function(
             self, 'CreateUser',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
             handler='createUser.handler',
+            # environment={
+            #     "USER_TABLE": userTable.table_name,
+            #     "AWS_REGION": self.region
+            # }
         )
+
+        # userTable.grant_full_access(create_user_lambda)
+
         # Create the Lambda function for creating a game
         create_game_lambda = _lambda.Function(
             self,
             "CreateGameLambda",
             runtime=_lambda.Runtime.PYTHON_3_8,
             handler="createGame.handler",
-            code=_lambda.Code.from_asset("lambda")
+            code=_lambda.Code.from_asset("lambda"),
+            # environment={
+            #     "USER_TABLE": userTable.table_name,
+            #     "GAME_TABLE": gameTable.table_name,
+            #     "AWS_REGION": self.region
+            # }
         )
+
+        # userTable.grant_full_access(create_game_lambda)
+        # gameTable.grant_full_access(create_game_lambda)
+        
 
         # Create the Lambda function for deleting a game
         delete_game_lambda = _lambda.Function(
@@ -34,8 +70,34 @@ class WordleCdkStack(Stack):
             "DeleteGameLambda",
             runtime=_lambda.Runtime.PYTHON_3_8,
             handler="deleteGame.handler",
-            code=_lambda.Code.from_asset("lambda")
+            code=_lambda.Code.from_asset("lambda"),
+            # environment={
+            #     "USER_TABLE": userTable.table_name,
+            #     "GAME_TABLE": gameTable.table_name,
+            #     "AWS_REGION": self.region
+            # }
         )
+
+        # userTable.grant_full_access(delete_game_lambda)
+        # gameTable.grant_full_access(delete_game_lambda)
+        
+
+        # Create the Lambda function for making a guess
+        guess_lambda = _lambda.Function(
+            self,
+            "GuessLambda",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="guess.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            # environment={
+            #     "USER_TABLE": userTable.table_name,
+            #     "GAME_TABLE": gameTable.table_name,
+            #     "AWS_REGION": self.region
+            # }
+        )
+
+        # userTable.grant_full_access(guess_lambda)
+        # gameTable.grant_full_access(guess_lambda)
 
         api = apigw.RestApi(
             self,
@@ -68,3 +130,10 @@ class WordleCdkStack(Stack):
         # Add a DELETE method to the `/users/{user_id}/games/{game_id}` resource and connect it to the delete game Lambda function
         delete_game_integration = apigw.LambdaIntegration(delete_game_lambda)
         games_resource.add_method("DELETE", delete_game_integration)
+
+        # Create the `/users/{user_id}/games/{game_id}/guess` resource
+        guess_resource = games_resource.add_resource("guess")
+
+        # Add a POST method to the `/users/{user_id}/games/{game_id}/guess` resource and connect it to the guess Lambda function
+        guess_integration = apigw.LambdaIntegration(guess_lambda)
+        guess_resource.add_method("POST", guess_integration)
